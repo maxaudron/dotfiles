8bd3941cbba9b5554ab3853b59e8b154c3d126b6
Max Audron <audron@cocaine.farm>
2022-02-03 09:53:59 +0100

    fix dit

--- src/.shell/functions/dit.sh
+++ src/.shell/functions/dit.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env sh
 
-if [ -f "$HOME/.local/bin/dit.sh" ]; then
+if [ -n "$(which dit)" ]; then
+    export DITSH_BASE="$HOME/repo"
+
     git() {
       if [ "$1" = "clone" ]; then
-        command "$HOME/.local/bin/dit.sh" $*
+        command dit $*
       else
         command git $*
       fi
     }
-    
-    export DITSH_URL=gitlab.com
-    export DITSH_BASE=$HOME/repo
-    export DITSH_SSH=true
 fi
