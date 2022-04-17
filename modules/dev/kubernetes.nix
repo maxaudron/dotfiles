1ae95149ffccc4c8294d495fe9655d7d00d970a9
Max Audron <audron@cocaine.farm>
2022-04-17 12:46:32 +0200

    add kubectx

--- modules/dev/kubernetes.nix
+++ modules/dev/kubernetes.nix
@@ -2,6 +2,6 @@
 
 {
   config = lib.mkIf config.home.dev.kubernetes {
-    home.packages = with pkgs; [ krew tanka kubectl helmfile kubernetes-helm ];
+    home.packages = with pkgs; [ krew tanka kubectl kubectx helmfile kubernetes-helm ];
   };
 }
