611843edcf662bf33cf74d555be2af51c0c04bfc
Max Audron <audron@cocaine.farm>
2022-04-19 15:16:21 +0200

    fix kubeconfig path

--- modules/shell/functions/kubeconfig.sh
+++ modules/shell/functions/kubeconfig.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-export KUBECONFIG="~/.kube/config:$(find ~/.kube/configs -type f | paste -sd ':' - )"
+export KUBECONFIG="$HOME/.kube/config:$(find ~/.kube/configs -type f | paste -sd ':' - )"
