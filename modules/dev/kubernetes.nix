7cc990d1f41baf2c0b1d55c772175bcac06ac14b
Max Audron <audron@cocaine.farm>
2022-04-19 15:01:09 +0200

    fix up kubectl config var and kubectx package

--- modules/dev/kubernetes.nix
+++ modules/dev/kubernetes.nix
@@ -1,7 +1,17 @@
 { config, lib, pkgs, ... }:
 
-{
+let
+
+in {
   config = lib.mkIf config.home.dev.kubernetes {
-    home.packages = with pkgs; [ krew tanka kubectl kubectx helmfile kubernetes-helm ];
+    home.packages = with pkgs; [
+      krew
+      tanka
+      kubectl
+      helmfile
+      kubernetes-helm
+
+      (callPackage ../../pkgs/kubectx {})
+    ];
   };
 }
--- modules/shell/functions/kubeconfig.sh
+++ modules/shell/functions/kubeconfig.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-#export KUBECONFIG="$(find ~/.kube/configs/ -type f | paste -sd ':' - )"
-
+export KUBECONFIG="~/.kube/config:$(find ~/.kube/configs -type f | paste -sd ':' - )"
