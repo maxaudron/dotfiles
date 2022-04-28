d0b655c6342dbe4ba9fcf6e8819938d4eee3da42
Max Audron <audron@cocaine.farm>
2022-04-28 13:38:05 +0200

    fix kiwi package

--- pkgs/kiwi/default.nix
+++ pkgs/kiwi/default.nix
@@ -6,12 +6,13 @@ buildPythonApplication {
   pname = "kiwi";
   version = "9.24.32";
 
-  propagatedBuildInputs = [ docopt lxml pyxattr requests pyyaml simplejson ];
+  propagatedBuildInputs =
+    [ setuptools docopt lxml pyxattr requests pyyaml simplejson ];
 
   src = python3Packages.fetchPypi {
-    format = "setuptools";
     pname = "kiwi";
     version = "9.24.32";
-    hash = "sha256:c3017a641b0ae6f2e61629ec452db918b11434fb6fc4d008a603ba5b6ca1f481";
+    hash =
+      "sha256:c3017a641b0ae6f2e61629ec452db918b11434fb6fc4d008a603ba5b6ca1f481";
   };
 }
