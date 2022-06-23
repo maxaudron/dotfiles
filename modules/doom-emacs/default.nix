9072780d3329ec8ccac88d58d8064d9975d86eb4
Max Audron <audron@cocaine.farm>
2022-06-23 08:58:35 +0200

    switch out emacs packages

--- modules/doom-emacs/default.nix
+++ modules/doom-emacs/default.nix
@@ -5,7 +5,7 @@ with lib;
 let
   conf = import ../config { inherit lib; };
   emacsPackage =
-    if pkgs.stdenv.isLinux then pkgs.emacsPgtkGcc else pkgs.emacs;
+    if pkgs.stdenv.isLinux then pkgs.emacsPgtkNativeComp else pkgs.emacs28NativeComp;
 in {
   home = {
     sessionPath = [ "${config.xdg.configHome}/emacs/bin" ];
