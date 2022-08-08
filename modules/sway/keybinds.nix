0ba5a590f98ad1406c1902c0b02b1c25cadd50f6
Max Audron <audron@cocaine.farm>
2022-08-08 11:50:28 +0200

    add obs keypad binds to sway

--- modules/sway/keybinds.nix
+++ modules/sway/keybinds.nix
@@ -18,6 +18,25 @@ in {
       "XF86AudioMute" = "exec xdotool keydown XF86AudioMute";
       "--release --locked XF86AudioMute" = "exec xdotool keyup XF86AudioMute";
 
+      "Control+Mod4+Mod1+Shift+1" = "exec xdotool keydown ctrl keydown super keydown alt keydown shift keydown 1";
+      "--release --locked Control+Mod4+Mod1+Shift+1" = "exec xdotool keyup ctrl keydown super keydown alt keydown shift keydown 1";
+      "Control+Mod4+Mod1+Shift+2" = "exec xdotool keydown ctrl keydown super keydown alt keydown shift keydown 2";
+      "--release --locked Control+Mod4+Mod1+Shift+2" = "exec xdotool keyup ctrl keydown super keydown alt keydown shift keydown 2";
+      "Control+Mod4+Mod1+Shift+3" = "exec xdotool keydown ctrl keydown super keydown alt keydown shift keydown 3";
+      "--release --locked Control+Mod4+Mod1+Shift+3" = "exec xdotool keyup ctrl keydown super keydown alt keydown shift keydown 3";
+      "Control+Mod4+Mod1+Shift+4" = "exec xdotool keydown ctrl keydown super keydown alt keydown shift keydown 4";
+      "--release --locked Control+Mod4+Mod1+Shift+4" = "exec xdotool keyup ctrl keydown super keydown alt keydown shift keydown 4";
+      "Control+Mod4+Mod1+Shift+5" = "exec xdotool keydown ctrl keydown super keydown alt keydown shift keydown 5";
+      "--release --locked Control+Mod4+Mod1+Shift+5" = "exec xdotool keyup ctrl keydown super keydown alt keydown shift keydown 5";
+      "Control+Mod4+Mod1+Shift+6" = "exec xdotool keydown ctrl keydown super keydown alt keydown shift keydown 6";
+      "--release --locked Control+Mod4+Mod1+Shift+6" = "exec xdotool keyup ctrl keydown super keydown alt keydown shift keydown 6";
+      "Control+Mod4+Mod1+Shift+7" = "exec xdotool keydown ctrl keydown super keydown alt keydown shift keydown 7";
+      "--release --locked Control+Mod4+Mod1+Shift+7" = "exec xdotool keyup ctrl keydown super keydown alt keydown shift keydown 7";
+      "Control+Mod4+Mod1+Shift+8" = "exec xdotool keydown ctrl keydown super keydown alt keydown shift keydown 8";
+      "--release --locked Control+Mod4+Mod1+Shift+8" = "exec xdotool keyup ctrl keydown super keydown alt keydown shift keydown 8";
+      "Control+Mod4+Mod1+Shift+9" = "exec xdotool keydown ctrl keydown super keydown alt keydown shift keydown 9";
+      "--release --locked Control+Mod4+Mod1+Shift+9" = "exec xdotool keyup ctrl keydown super keydown alt keydown shift keydown 9";
+
       # c floating_modifier "${mod}" normal
 
       # Exit and reload
