1d8f0a5f17559e353bf2d11b763a98895004d643
Max Audron <audron@cocaine.farm>
2022-10-01 17:26:57 +0200

    update factorio

--- modules/games/default.nix
+++ modules/games/default.nix
@@ -1,9 +1,16 @@
 { config, lib, pkgs, nixpkgs-master, system, ... }:
 
-let factorio = pkgs.master.factorio.override {
-      username = "anymax";
-      token = "e572bb639cecdfa8190a3b46860190";
-    };
-in {
-  home.packages = [ factorio ];
-}
+let
+  nixpkgs = import (fetchTarball {
+    url = "https://github.com/leifhelm/nixpkgs/archive/factorio.tar.gz";
+    sha256 = "0qrrg2i9l05i6ig7969wj1r1ccddb7na2486355410ci75xh4sy7";
+  });
+  pkgs = nixpkgs {
+    system = system;
+    config.allowUnfree = true;
+  };
+  factorio = pkgs.factorio.override {
+    username = "anymax";
+    token = "e572bb639cecdfa8190a3b46860190";
+  };
+in { home.packages = [ factorio ]; }
