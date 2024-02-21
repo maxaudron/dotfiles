1e4de4f4adc19179590b2d65c78591c05e3b2d6a
Max Audron <audron@cocaine.farm>
2024-02-21 12:34:28 +0100

    reenable bootstrapper

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,7 +3,7 @@ self: super:
 {
   stlink-tool = super.callPackage ./stlink-tool {};
   ansible-run = super.callPackage ./ansible-run {};
-  # bootstrap = super.callPackage ./bootstrap {};
+  bootstrap = super.callPackage ./bootstrap {};
   kubectx = super.callPackage ./kubectx {};
 
   libnpupnp = super.callPackage ./libnpupnp {};
