102dbb8af77cf9d85ad75947f439272539ba27be
Max Audron <audron@cocaine.farm>
2024-02-21 12:34:06 +0100

    fix fonts.packages rename

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -35,7 +35,7 @@ in {
     passExtensions.pass-update
   ];
 
-  fonts.packages = with pkgs; [ ibm-plex nerdfonts ];
+  fonts.fonts = with pkgs; [ ibm-plex nerdfonts ];
 
   # Enable zsh completion for system packages
   environment.pathsToLink = [ "/share/zsh" ];
