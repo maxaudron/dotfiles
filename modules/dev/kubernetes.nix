93d48286b736862bcae550f2ff7c9bfd804154cc
Max Audron <audron@cocaine.farm>
2024-08-09 16:00:20 +0200

    add gcp auth plugin

--- modules/dev/kubernetes.nix
+++ modules/dev/kubernetes.nix
@@ -20,7 +20,7 @@
       kube-review
       argocd
 
-      google-cloud-sdk
+      (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
       awscli2
       aws-adfs
 
