74d477959e410953fb998755d912da1a57219e2a
Max Audron <audron@cocaine.farm>
2025-03-25 10:41:38 +0100

    latex stuff

--- modules/latex/default.nix
+++ modules/latex/default.nix
@@ -4,9 +4,10 @@
   home.packages = with pkgs; [
     (texlive.combine {
       inherit (pkgs.texlive)
-        scheme-basic latexmk xetex beamer minted wrapfig ulem capt-of fvextra
+        scheme-small latexmk beamer minted wrapfig ulem capt-of fvextra
         fancyvrb upquote lineno catchfile xstring framed float datetime2
-        tracklang xkeyval animate zref media9 plex plex-otf fontspec moresize;
+        tracklang xkeyval animate zref media9 plex plex-otf fontspec moresize
+	ocgx2 pygmentex svg trimspaces transparent;
     })
   ];
 }
