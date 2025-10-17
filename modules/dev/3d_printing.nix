b77fc8c661995f2cc4bf2613ca42db5c9ca4c4df
Max Audron <audron@cocaine.farm>
2025-10-17 14:37:31 +0200

    back to upstream prusaslicer

--- modules/dev/3d_printing.nix
+++ modules/dev/3d_printing.nix
@@ -7,21 +7,7 @@ in
   config = lib.mkIf (!conf.os.work) {
     home.packages = with pkgs;
       [
-        # unstable.prusa-slicer
-        (unstable.prusa-slicer.overrideAttrs (prev: {
-          patches = [ ./fix-mmu.patch ];
-          version = "2.9.3-beta1";
-          src = fetchFromGitHub {
-            owner = "prusa3d";
-            repo = "PrusaSlicer";
-            hash = "sha256-oQdXRYrrcuxoVgKziiHjKffXhNBaamVsDy37FZw57LA=";
-            rev = "version_2.9.3-beta1";
-          };
-
-          buildInputs = prev.buildInputs ++ [
-            nlohmann_json
-          ];
-        }))
+        unstable.prusa-slicer
       ];
   };
 }
