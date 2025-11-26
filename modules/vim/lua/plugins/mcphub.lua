53d8650155171640a213c52d0c839367dcd0aac9
Max Audron <me@audron.dev>
2025-11-26 14:27:54 +0100

    add nvim keybind to open serena dash

--- modules/vim/lua/plugins/mcphub.lua
+++ modules/vim/lua/plugins/mcphub.lua
@@ -9,5 +9,6 @@ return {
 
 	keys = {
 		{ "<leader>am", "<cmd>MCPHub<cr>", desc = "Show MCPHub" },
+		{ "<leader>as", "<cmd>!open http://127.0.0.1:24282/dashboard/<cr>", desc = "Open Serena Dashboard" },
 	},
 }
