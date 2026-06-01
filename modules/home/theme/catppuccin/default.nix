6962cd4c80713b09c4c8edff48184b42d3d5291c
Max Audron <me@audron.dev>
2026-06-01 14:08:16 +0200

    cl

--- modules/home/theme/catppuccin/default.nix
+++ modules/home/theme/catppuccin/default.nix
@@ -23,7 +23,7 @@
             accent = "dark";
           };
         };
-        
+
         programs.noctalia-shell.settings.colorSchemes.predefinedScheme = "Catppuccin";
 
         programs.fish.interactiveShellInit = ''
