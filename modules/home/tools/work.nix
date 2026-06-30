d73988dafc3366ee3c3e29aca3a7daa829d0391d
Max Audron <me@audron.dev>
2026-06-30 12:07:39 +0200

    add vault setup for work

--- modules/home/tools/work.nix
+++ modules/home/tools/work.nix
@@ -22,6 +22,17 @@
       (google-cloud-sdk.withExtraComponents [ google-cloud-sdk.components.gke-gcloud-auth-plugin ])
       awscli2
       aws-adfs
+
+      vault
     ];
+
+    home.sessionVariables = {
+      VAULT_ADDR = "https://vault.de.clara.net";
+    };
+
+    programs.fish.functions = {
+      vkl = "vault login -method=ldap \"password=$(pass show work/maximilian.manz@mgt.de.clara.net | head -n1)\"";
+      vkv = "vault kv $argv[1] -mount=secret $argv[2..-1]";
+    };
   };
 }
