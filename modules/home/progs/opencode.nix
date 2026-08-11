7b50739a454dee27f1149676d3c361ea376af7f8
Max Audron <me@audron.dev>
2026-08-11 11:41:22 +0200

    opencode updates

--- modules/home/progs/opencode.nix
+++ modules/home/progs/opencode.nix
@@ -35,7 +35,7 @@
             npm = "@ai-sdk/openai-compatible";
             name = "clara";
             options = {
-              baseURL = "https://public.llm.de.clara.net";
+              baseURL = "https://ai-llm.prod.cke.de.clara.net";
             };
             models = {
               "claude-opus-4.8" = { };
@@ -46,19 +46,40 @@
               "claude-sonnet-4.6" = { };
               "claude-sonnet-4.7" = { };
               "claude-haiku-4.5" = { };
+              "gpt-5.6-lua" = { };
+              "gpt-5.6-terra" = { };
+              "gpt-5.6-sol" = { };
             };
           };
         };
         mcp = {
-          # git-search = {
-          #   type = "remote";
-          #   url = "https://git-search.prod.cke.de.clara.net/mcp";
-          #   enabled = true;
-          # };
+          litellm-dev = {
+            type = "remote";
+            url = "https://ai-llm.dev.cke.de.clara.net/mcp";
+            enabled = true;
+            headers = {
+              Authorization = "Bearer {env:LITELLM_VIRTUAL_KEY}";
+            };
+          };
+          llm-cloudatlas = {
+            type = "remote";
+            url = "https://ai-llm.dev.cke.de.clara.net/CloudAtlas_MCP/mcp";
+            enabled = true;
+          };
+          cloudatlas = {
+            type = "remote";
+            url = "https://cloudatlas-mcp.dev.cke.de.clara.net/";
+            enabled = true;
+          };
+          git-search = {
+            type = "remote";
+            url = "https://git-search.prod.cke.de.clara.net/mcp";
+            enabled = true;
+          };
           git-search-dev = {
             type = "remote";
             url = "https://git-search.dev.cke.de.clara.net/mcp";
-            enabled = true;
+            enabled = false;
           };
         };
       };
