e80dcd35eac4f135bed40fc67997355d68772e9b
Max Audron <me@audron.dev>
2026-08-11 14:44:42 +0200

    color fish shell ssh sesh

--- modules/home/core/shell/default.nix
+++ modules/home/core/shell/default.nix
@@ -69,7 +69,37 @@ in
     package = pkgs.unstable.fish;
     generateCompletions = false;
     interactiveShellInit = ''
-      set fish_greeting # Disable greeting
+      set fish_greeting # Disable greetinga
+
+
+      if set -q SSH_CONNECTION
+        set -l palette \
+            89b4fa \
+            74c7ec \
+            89dceb \
+            94e2d5 \
+            a6e3a1 \
+            f9e2af \
+            fab387 \
+            eba0ac \
+            f38ba8 \
+            cba6f7 \
+            f5c2e7 \
+            babbf1 \
+            a6d189 \
+            ef9f76 \
+            ea999c \
+            ca9ee6
+
+        # Hash the hostname, extract a decimal number from the hex digest
+        set -l hash_val (echo (hostname) | md5sum | cut -c 1-8)
+        set -l index (math "0x$hash_val % "(count $palette))
+
+        set --global hydro_color_pwd $fish_color_host
+      end
+
+      # Store the picked color
+      set -g fish_color_host $palette[(math $index + 1)]
 
       # Hydro
       set --global hydro_symbol_prompt '>'
